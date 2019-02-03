4a0c7f0e8f0017fb2fd716066da93c3503e46d47
Oshada Maheeshan <oshadamahee@gmail.com>
2019-02-03 17:28:36 +0530

    Initial Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'angularEx001-project'`, async(() => {
+  it(`should have as title 'My First App'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angularEx001-project');
+    expect(app.title).toEqual('My First App');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angularEx001-project!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to My First App');
   }));
 });
