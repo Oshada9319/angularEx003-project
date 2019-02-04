6bd0540378b76ad640b60a16d143eb652f5ec181
Oshada Maheeshan <oshadamahee@gmail.com>
2019-02-04 12:47:36 +0530

    add name tag component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { NamesComponent } from './names/names.component';
+import { NameTagComponent } from './name-tag/name-tag.component'
 
 @NgModule({
   declarations: [
     AppComponent,
+    NamesComponent,
+    NameTagComponent,
   ],
   imports: [
     BrowserModule,
